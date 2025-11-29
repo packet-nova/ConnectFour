e690512e1fed819b0155127410d64ccaacb04eb6
packet-nova <90776963+packet-nova@users.noreply.github.com>
2025-11-29 10:06:57 -0500

    Basic empty board rendered.

---
+++ Board.cs
@@ -0,0 +1,36 @@
+﻿namespace ConnectFour
+{
+    internal class Board
+    {
+        public static void RenderBoard()
+        {
+            //char right = '\u22A2';
+            char right = '\u251C';
+            char left = '\u2524';
+            char down = '\u252C';
+            char up = '\u2534';
+            char topLeft = '\u250C';
+            char topRight = '\u2510';
+            char bottomLeft = '\u2514';
+            char bottomRight = '\u2518';
+
+
+            Console.WriteLine($"""
+            {topLeft}---{down}---{down}---{down}---{down}---{down}---{down}---{topRight}
+            |   |   |   |   |   |   |   |
+            {right}---+---+---+---+---+---+---{left}
+            |   |   |   |   |   |   |   |
+            {right}---+---+---+---+---+---+---{left}
+            |   |   |   |   |   |   |   |
+            {right}---+---+---+---+---+---+---{left}
+            |   |   |   |   |   |   |   |
+            {right}---+---+---+---+---+---+---{left}
+            |   |   |   |   |   |   |   |
+            {right}---+---+---+---+---+---+---{left}
+            |   |   |   |   |   |   |   |
+            {bottomLeft}---{up}---{up}---{up}---{up}---{up}---{up}---{bottomRight}
+            """
+                );
+        }
+    }
+}
---
+++ ConnectFour.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConnectFour
+{
+    internal class ConnectFour
+    {
+    }
+
+    public enum Player { Empty, Red, Yellow }
+}
