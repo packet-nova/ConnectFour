e023babbb73dc7e4defd55fed251131708dd9cb8
packet-nova <90776963+packet-nova@users.noreply.github.com>
2025-11-29 22:00:07 -0500

    local vars to fields

--- Board.cs
+++ Board.cs
@@ -3,24 +3,26 @@
     internal class Board
     {
         private readonly Token[,] _tokens = new Token[6, 7];
+        private const char Right = '\u251C';
+        private const char Down = '\u252C';
+        private const char Left = '\u2524';
+        private const char Up = '\u2534';
+        private const char TopLeft = '\u250C';
+        private const char TopRight = '\u2510';
+        private const char BottomLeft = '\u2514';
+        private const char BottomRight = '\u2518';
+        private readonly string _divider = $"{Right}---+---+---+---+---+---+---{Left}";
+        private readonly string _topBorder = $"{TopLeft}---{Down}---{Down}---{Down}---{Down}---{Down}---{Down}---{TopRight}";
+        private readonly string _bottomBorder = $"{BottomLeft}---{Up}---{Up}---{Up}---{Up}---{Up}---{Up}---{BottomRight}";
 
         public void RenderBoard()
         {
-            char right = '\u251C';
-            char left = '\u2524';
-            char down = '\u252C';
-            char up = '\u2534';
-            char topLeft = '\u250C';
-            char topRight = '\u2510';
-            char bottomLeft = '\u2514';
-            char bottomRight = '\u2518';
-            string divider = $"{right}---+---+---+---+---+---+---{left}";
-            string topBorder = $"{topLeft}---{down}---{down}---{down}---{down}---{down}---{down}---{topRight}";
-            string bottomBorder = $"{bottomLeft}---{up}---{up}---{up}---{up}---{up}---{up}---{bottomRight}";
+
+
 
             Console.ForegroundColor = ConsoleColor.Blue;
             
-            Console.WriteLine(topBorder);
+            Console.WriteLine(_topBorder);
             for (int row = 0; row < _tokens.GetLength(0); row++)
             {
                 for (int column = 0; column < _tokens.GetLength(1); column++)
@@ -32,10 +34,10 @@
                 Console.WriteLine("|");
                 if (row < (_tokens.GetLength(0) - 1))
                 {
-                    Console.WriteLine(divider);
+                    Console.WriteLine(_divider);
                 }
             }
-            Console.WriteLine(bottomBorder);
+            Console.WriteLine(_bottomBorder);
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("  1   2   3   4   5   6   7");
             
